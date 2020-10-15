ccac8ccb95c83a5f5e23c43869b4bab248e7e88a
大谷 涼 <16020@kauth.local>
2020-10-15 09:30:50 +0900

    Bがキャラ追加

---
+++ team4/最強.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class 最強 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class 最強 extends Actor
+{
+    /**
+     * Act - do whatever the 最強 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
